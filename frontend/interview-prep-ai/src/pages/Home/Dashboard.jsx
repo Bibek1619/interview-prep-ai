eeb02d51cc195999dde1fc0e05139e6d96804564
bee_bek_wagle <128395029+Bibek1619@users.noreply.github.com>
2026-02-20 08:47:14 +0545

    build

--- frontend/interview-prep-ai/src/pages/Home/Dashboard.jsx
+++ frontend/interview-prep-ai/src/pages/Home/Dashboard.jsx
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 
 import DashboardLayout from "./../../components/layouts/DashboardLayout";
 import { useNavigate } from "react-router-dom";
-import axiosInstance from "../../utils/axiosInstance";
+import { axiosInstance } from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
 
 import moment from "moment";
